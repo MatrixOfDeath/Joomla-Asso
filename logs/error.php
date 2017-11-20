e6b6ee51002238096ea1999ac4816496227fcb23
Karim BOUBRIT <boubrit.karim@gmail>
2017-11-20 17:57:50 +0100

    Adding existing project

---
+++ logs/error.php
@@ -0,0 +1,16 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-10-08 19:25:16 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2014-10-08T19:25:16+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-10T15:11:50+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-11T00:03:29+00:00	INFO ::1	joomlafailure	Empty password not allowed
+2014-10-24T19:31:52+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-24T19:31:59+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-24T19:32:02+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-01-18T13:02:34+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-01-18T13:02:39+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-01-18T13:02:45+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-01-18T13:02:52+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
